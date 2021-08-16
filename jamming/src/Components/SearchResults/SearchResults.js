b2752147ba7a9799eaa566cf7547263fcb37d38e
Piwero <piwero@gmail.com>
2021-08-16 13:01:02 +0100

    add components for web api

--- jamming/src/Components/SearchResults/SearchResults.js
+++ jamming/src/Components/SearchResults/SearchResults.js
@@ -1,14 +1,18 @@
-import React from "react";
-import "./SearchResults.css";
-import TrackList  from "../TrackList/TrackList";
+import React from 'react';
 
-export class SearchResults extends React.Component{
-    render()
-{
-    return(
-        <div className="SearchResults">
-            <h2>Results</h2>
-            < TrackList />
-        </div>
-    )
-}}
+import './SearchResults.css';
+
+import TrackList from '../TrackList/TrackList';
+
+class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} />
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
